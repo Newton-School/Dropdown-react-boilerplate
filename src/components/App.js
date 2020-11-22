8d8ef97925da4590e0e03061f781713a816209c7
Shreya Chandra <shreya.chandra96@gmail.com>
2020-11-23 03:38:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,108 +1,108 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
-// Do not alter the mentiones states variable and values inside it as test cases depend on it.
-	const states = [{
-		name : "Madhya Pradesh",
-		cities :[{
-			name : "Indore",
-			towns :[{
-				name : "Mhow"
-			},{
-				name : "Dewas"
-			}]
+// Do not alter the states const and values inside it.
+const states = [{
+	name : "Madhya Pradesh",
+	cities :[{
+		name : "Indore",
+		towns :[{
+			name : "Mhow"
 		},{
-			name : "Bhopal",
-			towns :[{
-				name : "Manit"
-			},{
-				name: "Berasia"
-			}]
-		},{
-			name : "Gwalior",
-			towns :[{
-				name : "Ajaypur"
-			}]
+			name : "Dewas"
 		}]
 	},{
-		name : "Jharkhand",
-		cities :[{
-			name : "Dhanbad",
-			towns :[{
-				name : "IIT(ISM) Dhanbad"
-			},{
-				name : "Hirapur"
-			}]
+		name : "Bhopal",
+		towns :[{
+			name : "Manit"
 		},{
-			name : "Wasseypur",
-			towns :[{
-				name : "Sardar khan's"
-			},{
-				name : "Faizal khan's"
-			}]
-		},{
-			name : "Mirzapur",
-			towns :[{
-				name : "Kaleen bhaiya's"
-			},{
-				name : "Guddu bhaiya's"
-			}]
+			name: "Berasia"
 		}]
 	},{
-		name : "Assam",
-		cities :[{
-			name : "Guwhati",
-			towns :[{
-				name : "Amin"
-			},{
-				name : "Jalah"
-			}]
+		name : "Gwalior",
+		towns :[{
+			name : "Ajaypur"
+		}]
+	}]
+},{
+	name : "Jharkhand",
+	cities :[{
+		name : "Dhanbad",
+		towns :[{
+			name : "IIT(ISM) Dhanbad"
 		},{
-			name : "Jungle1",
-			towns :[{
-				name : "Tiger found at IIT Guwahati"
-			},{
-				name : "Leopard found in IIT Guwahati"
-			}]
-		},{
-			name : "Tezpur",
-			towns :[{
-				name : "Dibrugarh"
-			},{
-				name : "Silchar"
-			}]
+			name : "Hirapur"
 		}]
 	},{
-		name : "Bihar",
-		cities :[{
-			name : "Patna",
-			towns :[{
-				name : "Sonpur"
-			},{
-				name : "Maner"
-			}]
+		name : "Wasseypur",
+		towns :[{
+			name : "Sardar khan's"
 		},{
-			name : "Gaya",
-			towns :[{
-				name : "Bakraur"
-			},{
-				name : "Barachatti"
-			}]	
-		},{
-			name : "Darbhanga",
-			towns :[{
-				name : "Singhwara"
-			},{
-				name : "Jale"
-			}]
+			name : "Faizal khan's"
 		}]
-	}];
+	},{
+		name : "Mirzapur",
+		towns :[{
+			name : "Kaleen bhaiya's"
+		},{
+			name : "Guddu bhaiya's"
+		}]
+	}]
+},{
+	name : "Assam",
+	cities :[{
+		name : "Guwhati",
+		towns :[{
+			name : "Amin"
+		},{
+			name : "Jalah"
+		}]
+	},{
+		name : "Jungle1",
+		towns :[{
+			name : "Tiger found at IIT Guwahati"
+		},{
+			name : "Leopard found in IIT Guwahati"
+		}]
+	},{
+		name : "Tezpur",
+		towns :[{
+			name : "Dibrugarh"
+		},{
+			name : "Silchar"
+		}]
+	}]
+},{
+	name : "Bihar",
+	cities :[{
+		name : "Patna",
+		towns :[{
+			name : "Sonpur"
+		},{
+			name : "Maner"
+		}]
+	},{
+		name : "Gaya",
+		towns :[{
+			name : "Bakraur"
+		},{
+			name : "Barachatti"
+		}]	
+	},{
+		name : "Darbhanga",
+		towns :[{
+			name : "Singhwara"
+		},{
+			name : "Jale"
+		}]
+	}]
+}];
 
 
 function App() 
 {
 	return (
-	    <div id="main"></div>
+		<div id="main"></div>
 	);
 }
 
